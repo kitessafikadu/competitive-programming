02324ebc5bae59b5b581c0cde125aaf63d7e5eef
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-02-20 23:39:24 +0300

    Created 'Image Smoother' by A2SV Hub

---
+++ 20-Feb-2025/Image Smoother 277605.py
@@ -0,0 +1,26 @@
+# Problem: Image Smoother - https://leetcode.com/problems/image-smoother/description/
+
+class Solution(object):
+    def imageSmoother(self, img):
+        """
+        :type img: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        m, n = len(img), len(img[0])
+        result = [[0] * n for _ in range(m)]
+        
+        for i in range(m):
+            for j in range(n):
+                total, count = 0, 0
+                
+                for x in range(i - 1, i + 2):
+                    for y in range(j - 1, j + 2):
+                        if 0 <= x < m and 0 <= y < n:
+                            total += img[x][y]
+                            count += 1
+                
+                result[i][j] = total // count
+        
+        return result
+
+        
