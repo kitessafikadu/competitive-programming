9906e4bbd8189334745dba3f3cb49bf6de6acb6f
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-02-21 21:34:23 +0300

    Created 'Rotate Image' by A2SV Hub

---
+++ 21-Feb-2025/Rotate Image 279436.py
@@ -0,0 +1,10 @@
+# Problem: Rotate Image - https://leetcode.com/problems/rotate-image/
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        matrix[:] = [list(row) for row in zip(*matrix[::-1])]
+        return matrix
+        
