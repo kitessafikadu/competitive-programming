df879bcdde2d76f231aced8854414e6a9e4e4bab
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-02-21 23:48:14 +0300

    Created 'Palindrome Number' by A2SV Hub

---
+++ 21-Feb-2025/Palindrome Number 279985.py
@@ -0,0 +1,7 @@
+# Problem: Palindrome Number - https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        str_x = str(x)
+        return str_x == str_x[::-1]
+        
