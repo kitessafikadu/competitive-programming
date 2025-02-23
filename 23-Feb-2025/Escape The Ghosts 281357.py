79f069826523c75c55f3fa71e7ffd3c28e07d35a
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-02-23 23:57:29 +0300

    Created 'Escape The Ghosts' by A2SV Hub

---
+++ 23-Feb-2025/Escape The Ghosts 281357.py
@@ -0,0 +1,12 @@
+# Problem: Escape The Ghosts - https://leetcode.com/problems/escape-the-ghosts/
+
+class Solution:
+    def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
+        my_distance = abs(target[0]) + abs(target[1])
+
+        for ghost in ghosts:
+            ghost_distance = abs(ghost[0] - target[0]) + abs(ghost[1] - target[1])
+            if ghost_distance <= my_distance:
+                return False
+        return True
+        
