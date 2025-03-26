d1a54dbc39bb4aa779dc75f211d880be609001da
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-03-26 22:45:39 +0300

    Created 'Maximum subarray' by A2SV Hub

---
+++ 26-Mar-2025/Maximum subarray 319226.py
@@ -0,0 +1,15 @@
+# Problem: Maximum subarray - https://leetcode.com/problems/maximum-subarray/
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = float('-inf')
+        cur_sum = 0
+
+        for num in nums:
+            cur_sum += num
+            max_sum = max(max_sum,cur_sum)
+            if cur_sum < 0:
+                cur_sum = 0
+
+        return max_sum
+        
