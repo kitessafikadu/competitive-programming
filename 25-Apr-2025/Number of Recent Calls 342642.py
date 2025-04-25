2a78bbabfd4db06c25d1723a32dae823d4c71788
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-04-25 22:06:22 +0300

    Created 'Number of Recent Calls' by A2SV Hub

---
+++ 25-Apr-2025/Number of Recent Calls 342642.py
@@ -0,0 +1,17 @@
+# Problem: Number of Recent Calls - https://leetcode.com/problems/number-of-recent-calls/
+
+class RecentCounter:
+
+    def __init__(self):
+        self.times=deque()
+        
+    def ping(self, t: int) -> int:
+        self.times.append(t)
+        while self.times and self.times[0] < t-3000:
+            self.times.popleft()
+        return len(self.times)
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
