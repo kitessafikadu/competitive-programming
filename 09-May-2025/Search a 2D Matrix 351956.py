a12cd3147d758eaceea67eb1fda6f75d5f086a9f
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-05-09 22:12:19 +0300

    Created 'Search a 2D Matrix' by A2SV Hub

---
+++ 09-May-2025/Search a 2D Matrix 351956.py
@@ -0,0 +1,9 @@
+# Problem: Search a 2D Matrix - https://leetcode.com/problems/search-a-2d-matrix/
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for row in matrix:
+            if target in row:
+                return True
+        return False
+        
