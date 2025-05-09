b45a77c777a518981f12d3db2be282990b70dfcd
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-05-09 23:12:50 +0300

    Created 'Arranging Coins' by A2SV Hub

---
+++ 09-May-2025/Arranging Coins 352036.py
@@ -0,0 +1,16 @@
+# Problem: Arranging Coins - https://leetcode.com/problems/arranging-coins/description/
+
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        left,right=0,n
+        while left <= right:
+            mid=(left+right)//2
+            total=mid*(mid+1)//2
+            if total==n:
+                return mid
+            elif total < n:
+                left=mid+1
+            else:
+                right=mid-1
+
+        return right
